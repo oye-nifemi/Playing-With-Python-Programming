ac4e28b1f3451b4ec685c94999f4d94f50f662e7
oye-nifemi <nifoyewole@gmail.com>
2022-11-06 16:48:25 +0100

    Loading data with pandas

---
+++ loading_data_with_pandas.py
@@ -0,0 +1,4 @@
+import pandas as pd
+csv_path = ("C:/Users/nifoy/Desktop/IBM Full-stack Software Developer Professional Certificate/Python for Data Science, AI & Development/Playing-With-Python-Programming/Books.csv")
+df = pd.read_csv(csv_path)
+df.head()
